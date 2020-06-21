c8e5a06a50be7afee5d9952e3d802c7d918ab301
Nikolay <koliahndeko@gmail.com>
2020-06-21 21:41:15 +0300

    addedd router-links to sidebar

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <router-link 
+        v-for="link in links" :key="link.title"
+        tag="li" 
+        active-class="active" 
+        :to="link.url" 
+        exact>
+        <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  data () {
+    return {
+       links: [{
+           title: 'Счет',
+           url: '/'
+       },{
+           title: 'История',
+           url: '/history'
+       },{
+           title: 'Планирование',
+           url: '/planning'
+       },{
+           title: 'Новая запись',
+           url: '/record'
+       },{
+           title: 'Категории',
+           url: '/categories'
+       }] 
+    }
+  }
+};
+</script>
