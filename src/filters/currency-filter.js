a236990203d985887e643c8be02087ae28954a58
Nikolay <koliahndeko@gmail.com>
2020-06-25 21:37:21 +0300

    added categories create structure

--- src/filters/currency-filter.js
+++ src/filters/currency-filter.js
@@ -1,5 +1,5 @@
 export default function currencyFilter(value, currency = "UAH") {
-    return new Intl.NumberFormat('ua-UA', {
+    return new Intl.NumberFormat('ru-RU', {
         style: 'currency',
         currency
     }).format(value)
