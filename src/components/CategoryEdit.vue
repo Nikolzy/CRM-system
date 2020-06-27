39162ebc4b427f81abc1519f677841075d611dc5
Nikolay <koliahndeko@gmail.com>
2020-06-27 16:25:48 +0300

    connected edit category to database

--- src/components/CategoryEdit.vue
+++ src/components/CategoryEdit.vue
@@ -5,24 +5,48 @@
         <h4>Редактировать</h4>
       </div>
 
-      <form>
+      <form @submit.prevent="submitHandler">
         <div class="input-field">
-          <select>
-            <option>Category</option>
+          <select ref="select" v-model="current">
+            <option 
+              v-for="c of categories" 
+              :key="c.id" 
+              :value="c.id"
+            >{{c.title}}</option>
           </select>
           <label>Выберите категорию</label>
         </div>
 
         <div class="input-field">
-          <input type="text" id="name" />
+          <input i
+            d="name" 
+            type="text" 
+            v-model="title"
+            :class="{'invalid': $v.title.$dirty && !$v.title.required}"
+          />
           <label for="name">Название</label>
-          <span class="helper-text invalid">TITLE</span>
+          <span 
+            v-if="$v.title.$dirty && !$v.title.required"
+            class="helper-text invalid"
+          >
+            Введите название категории
+          </span>
         </div>
 
         <div class="input-field">
-          <input id="limit" type="number" />
+          <input 
+            id="limit" 
+            type="number" 
+            v-model.number="limit" 
+            :class="{'invalid': $v.limit.$dirty && !$v.limit.minValue}"
+          />
           <label for="limit">Лимит</label>
-          <span class="helper-text invalid">LIMIT</span>
+          <span 
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+            class="helper-text invalid"
+          >
+            Минимальное значение {{$v.limit.$params.minValue.min}}
+          </span>
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -33,3 +57,74 @@
     </div>
   </div>
 </template>
+
+<script>
+import {
+  required,
+  minValue,
+} from "../../node_modules/vuelidate/lib/validators";
+
+export default {
+  props: {
+    categories: Array
+  },
+  data: () => ({
+    select: null,
+    title: "",
+    limit: 100,
+    current: null
+  }),
+  validations: {
+    title: {
+      required,
+    },
+    limit: {
+      minValue: minValue(100),
+    },
+  },
+  watch: {
+    current(catId) {
+      const { title, limit} = this.categories.find(el => el.id === catId);
+
+      this.current = catId;
+      this.title = title;
+      this.limit = limit;
+    }
+  },
+  methods: {
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      try {
+        const categoryData = {
+          id: this.current,
+          title: this.title,
+          limit: this.limit
+        }
+        await this.$store.dispatch('updateCategory', categoryData);
+        this.$message('Категория успешно обновлена');
+        this.$emit('upadated', categoryData);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  },
+  created() {
+    const { id, title, limit } = this.categories[0];
+    this.current = id;
+    this.title = title;
+    this.limit = limit;
+  },
+  mounted() {
+    this.select = window.M.FormSelect.init(this.$refs.select, {isMultiple: false})
+    window.M.updateTextFields();
+  },
+  destroyed() {
+    if (this.select && this.select.destroy) {
+      this.select.destroy();
+    }
+  }
+}
+</script>
--- src/store/category.js
+++ src/store/category.js
@@ -2,6 +2,27 @@ import firebase from './../../../node_modules/firebase/app'
 
 export default {
     actions: {
+        async fetchCategories({ commit, dispatch }) {
+            try {
+                const uid = await dispatch('getUid');
+                const category = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {};
+
+                return Object.keys(category).map(id => ({...category[id], id }))
+
+            } catch (e) {
+                commit('setError', e);
+                throw e;
+            }
+        },
+        async updateCategory({ commit, dispatch }, { title, limit, id }) {
+            try {
+                const uid = await dispatch('getUid');
+                await firebase.database().ref(`/users/${uid}/categories`).child(id).update({ title, limit });
+            } catch (e) {
+                commit('setError', e);
+                throw e;
+            }
+        },
         async createCategory({ commit, dispatch }, { title, limit }) {
             try {
                 const uid = await dispatch('getUid');
