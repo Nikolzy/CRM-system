f30bb5a4d2a002802a5e06a5af754828ac0ffcfb
Nikolay <koliahndeko@gmail.com>
2020-06-27 22:37:51 +0300

    added validation to records component

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -4,38 +4,79 @@
       <h3>Новая запись</h3>
     </div>
 
-    <form class="form">
+    <Loader v-if="loading"/>
+
+    <p class="center" v-else-if="!categories.length">
+      Категорий пока нет. <router-link to='/categories'>Добавитьновую  категорию</router-link>
+    </p>
+
+    <form class="form" @submit.prevent="submitHandler" v-else>
       <div class="input-field">
-        <select>
-          <option>name cat</option>
+        <select ref="select" v-model="category">
+          <option 
+              v-for="c of categories" 
+              :key="c.id" 
+              :value="c.id"
+            >{{c.title}}</option>
         </select>
         <label>Выберите категорию</label>
       </div>
 
       <p>
         <label>
-          <input class="with-gap" name="type" type="radio" value="income" />
+          <input 
+            class="with-gap" 
+            name="type" 
+            type="radio" 
+            value="income" 
+            v-model="type"
+          />
           <span>Доход</span>
         </label>
       </p>
 
       <p>
         <label>
-          <input class="with-gap" name="type" type="radio" value="outcome" />
+          <input 
+            class="with-gap" 
+            name="type" 
+            type="radio" 
+            value="outcome" 
+            v-model="type"
+          />
           <span>Расход</span>
         </label>
       </p>
 
       <div class="input-field">
-        <input id="amount" type="number" />
+        <input 
+          id="amount" 
+          type="number" 
+          v-model.number="amount"
+          :class="{'invalid': ($v.amount.$dirty && !$v.amount.required) || ($v.amount.$dirty && !$v.amount.minValue)}"
+        />
         <label for="amount">Сумма</label>
-        <span class="helper-text invalid">amount пароль</span>
+          <span 
+            v-if="($v.amount.$dirty && !$v.amount.required) || ($v.amount.$dirty && !$v.amount.minValue)"
+            class="helper-text invalid"
+          >
+            Минимальное значение {{$v.amount.$params.minValue.min}}
+          </span>
       </div>
 
       <div class="input-field">
-        <input id="description" type="text" />
+        <input 
+          id="description" 
+          type="text" 
+          v-model="description"
+          :class="{'invalid': $v.description.$dirty && !$v.description.required}"
+        />
         <label for="description">Описание</label>
-        <span class="helper-text invalid">description пароль</span>
+        <span 
+            v-if="$v.description.$dirty && !$v.description.required"
+            class="helper-text invalid"
+          >Введите описание
+        </span>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -45,3 +86,51 @@
     </form>
   </div>
 </template>
+
+<script>
+import { required, minValue } from '../../node_modules/vuelidate/lib/validators';
+
+export default {
+  name: 'Record',
+  data: () => ({
+    loading: true,
+    categories: [],
+    select: null,
+    category: null,
+    type: 'outcome',
+    amount: 1,
+    description: ''
+  }),
+  validations: {
+    amount: {required, minValue: minValue(1)},
+    description: {required}
+  },
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+    }
+  },
+  async mounted(){
+    this.categories = await this.$store.dispatch('fetchCategories');
+    this.loading = false;
+
+    if (this.categories.length) {
+      this.category = this.categories[0].id
+    }
+    setTimeout(() => {
+      this.select = window.M.FormSelect.init(this.$refs.select, {isMultiple: false});
+      window.M.updateTextFields();
+    },0);
+    
+  },
+  destroyed() {
+    if (this.select && this.select.destroy) {
+      this.select.destroy();
+    }
+  }
+}
+</script>
